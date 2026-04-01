ccc5d1b07a3bf04e16dca0fc50b6c29205aea4a3
YassaTamer <yassashahat5@gmail.com>
2026-04-01 16:19:10 +0200

    feat: Implement admin profile management with Bloc architecture

--- lib/features/admin/profile/presentation/views/profile_view.dart
+++ lib/features/admin/profile/presentation/views/profile_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mosahem/core/constants/app_assets.dart';
 import 'package:mosahem/core/constants/app_colors.dart';
 import 'package:mosahem/core/widgets/custom_text.dart';
+import 'package:mosahem/features/admin/profile/logic/cubit/profile_cubit.dart';
+import 'package:mosahem/features/admin/profile/logic/cubit/profile_state.dart';
 import 'package:mosahem/features/admin/profile/presentation/views/edit_profile_view.dart';
 import 'package:mosahem/features/admin/profile/presentation/views/recent_opportunities_view.dart';
 import 'package:mosahem/features/admin/profile/presentation/views/recent_organizations_view.dart';
@@ -12,163 +15,206 @@ import 'package:mosahem/features/admin/profile/presentation/views/total_voluntee
 import 'package:mosahem/features/admin/profile/presentation/widgets/category_container_profile.dart';
 import 'package:mosahem/features/admin/profile/presentation/widgets/profile_clipper_widget.dart';
 
-class AdminProfileView extends StatelessWidget {
+class AdminProfileView extends StatefulWidget {
   const AdminProfileView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColors.primaryLightBlue,
-        title: CustomText(
-          'Mosahem',
-          fontWeight: FontWeight.bold,
-          color: AppColors.primary,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => EditProfileView()),
-              );
-            },
-            icon: Image.asset(AppAssets.editProfileIcon, width: 24, height: 24),
-          ),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SettingsAndActivityView(),
-                ),
-              );
-            },
-            icon: Image.asset(AppAssets.settingsIcon, width: 24, height: 24),
-          ),
-        ],
-      ),
+  State<AdminProfileView> createState() => _AdminProfileViewState();
+}
 
-      body: ListView(
-        children: [
-          //*** Container of admin info ***
-          Align(
-            alignment: AlignmentDirectional.topStart,
-            child: SizedBox(
-              width: 320,
-              height: 130,
-              child: ClipPath(
-                clipper: ProfileClipper(),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-                  decoration: BoxDecoration(color: AppColors.primary),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: AppColors.mustardYellow,
-                            width: 3,
-                          ),
-                        ),
-                        child: CircleAvatar(
-                          backgroundColor: AppColors.white,
-                          radius: 38,
-                          child: ClipOval(
-                            child: Image.asset(
-                              "assets/images/splash_logo.png",
-                              width: 100,
-                              height: 100,
-                            ),
-                          ),
-                        ),
+class _AdminProfileViewState extends State<AdminProfileView> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProfileCubit>().getMyProfile();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) {
+        if (state is ProfileLoading) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (state is ProfileError) {
+          return Scaffold(body: Center(child: Text(state.message)));
+        }
+
+        if (state is ProfileSuccess) {
+          final user = state.user;
+
+          return Scaffold(
+            backgroundColor: AppColors.white,
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: AppColors.primaryLightBlue,
+              title: CustomText(
+                'Mosahem',
+                fontWeight: FontWeight.bold,
+                color: AppColors.primary,
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditProfileView(),
                       ),
-                      const SizedBox(width: 12),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                    );
+                  },
+                  icon: Image.asset(
+                    AppAssets.editProfileIcon,
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SettingsAndActivityView(),
+                      ),
+                    );
+                  },
+                  icon: Image.asset(
+                    AppAssets.settingsIcon,
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
+              ],
+            ),
+
+            body: ListView(
+              children: [
+                //*** Container of admin info ***
+                Align(
+                  alignment: AlignmentDirectional.topStart,
+                  child: SizedBox(
+                    width: 320,
+                    height: 130,
+                    child: ClipPath(
+                      clipper: ProfileClipper(),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 20,
+                        ),
+                        decoration: BoxDecoration(color: AppColors.primary),
+                        child: Row(
                           children: [
-                            CustomText(
-                              "Betty Bassem",
-                              color: AppColors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            SizedBox(height: 5),
-                            Row(
-                              children: [
-                                Image.asset(AppAssets.phoneIcon),
-                                SizedBox(width: 10),
-                                CustomText(
-                                  "01225256162",
-                                  color: AppColors.white,
-                                  fontSize: 15,
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: AppColors.mustardYellow,
+                                  width: 3,
                                 ),
-                              ],
+                              ),
+                              child: CircleAvatar(
+                                backgroundColor: AppColors.white,
+                                radius: 38,
+                                child: ClipOval(
+                                  child: Image.asset(
+                                    "assets/images/splash_logo.png",
+                                    width: 100,
+                                    height: 100,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CustomText(
+                                    user.fullName,
+                                    color: AppColors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      Image.asset(AppAssets.phoneIcon),
+                                      SizedBox(width: 10),
+                                      CustomText(
+                                        user.phoneNumber,
+                                        color: AppColors.white,
+                                        fontSize: 15,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
+
+                //*** Divider ***
+                SizedBox(height: 10),
+                Divider(
+                  thickness: 1,
+                  color: AppColors.primaryDark,
+                  endIndent: 10,
+                  indent: 10,
+                ),
+
+                //*** Organizations container ***
+                CategoryContainerProfile(
+                  containerName: "Organizations",
+                  containerNumber: 800,
+                  totalButtonName: "total organizations",
+                  recentButtonName: "recent organizations",
+                  containerImage: AppAssets.organizationIcon2,
+                  leftPaddingImage: 4,
+                  navigationTotalPage: TotalOrganizationsView(),
+                  navigationRecentPage: RecentOrganizationsView(),
+                ),
+
+                SizedBox(height: 10),
+
+                //*** Opportunity container ***
+                CategoryContainerProfile(
+                  containerName: "Opportunities",
+                  containerNumber: 105,
+                  totalButtonName: "total opportunities",
+                  recentButtonName: "recent opportunities",
+                  containerImage: AppAssets.opportunityIcon,
+                  navigationTotalPage: TotalOpportunitiesView(),
+                  navigationRecentPage: RecentOpportunitiesView(),
+                ),
+
+                SizedBox(height: 10),
+
+                //*** Volunteer container ***
+                CategoryContainerProfile(
+                  containerName: "Volunteers",
+                  containerNumber: 1000,
+                  totalButtonName: "total volunteers",
+                  recentButton: false,
+                  containerImage: AppAssets.volunteerIcon2,
+                  leftPaddingImage: 95,
+                  navigationTotalPage: TotalVolunteersView(),
+                  recentPage: false,
+                ),
+              ],
             ),
-          ),
-
-          //*** Divider ***
-          SizedBox(height: 10),
-          Divider(
-            thickness: 1,
-            color: AppColors.primaryDark,
-            endIndent: 10,
-            indent: 10,
-          ),
-
-          //*** Organizations container ***
-          CategoryContainerProfile(
-            containerName: "Organizations",
-            containerNumber: 800,
-            totalButtonName: "total organizations",
-            recentButtonName: "recent organizations",
-            containerImage: AppAssets.organizationIcon2,
-            leftPaddingImage: 4,
-            navigationTotalPage: TotalOrganizationsView(),
-            navigationRecentPage: RecentOrganizationsView(),
-          ),
-
-          SizedBox(height: 10),
-
-          //*** Opportunity container ***
-          CategoryContainerProfile(
-            containerName: "Opportunities",
-            containerNumber: 105,
-            totalButtonName: "total opportunities",
-            recentButtonName: "recent opportunities",
-            containerImage: AppAssets.opportunityIcon,
-            navigationTotalPage: TotalOpportunitiesView(),
-            navigationRecentPage: RecentOpportunitiesView(),
-          ),
-
-          SizedBox(height: 10),
-
-          //*** Volunteer container ***
-          CategoryContainerProfile(
-            containerName: "Volunteers",
-            containerNumber: 1000,
-            totalButtonName: "total volunteers",
-            recentButton: false,
-            containerImage: AppAssets.volunteerIcon2,
-            leftPaddingImage: 95,
-            navigationTotalPage: TotalVolunteersView(),
-            recentPage: false,
-          ),
-        ],
-      ),
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:mosahem/core/network/dio_helper.dart';
+import 'package:mosahem/features/admin/profile/data/api/profile_api_service.dart';
+import 'package:mosahem/features/admin/profile/data/repository/profile_repository.dart';
+import 'package:mosahem/features/admin/profile/logic/cubit/profile_cubit.dart';
 import 'package:mosahem/features/organization/createOpp/data/api/create_opportunity_api_service.dart';
 import 'package:mosahem/features/organization/createOpp/data/repository/create_opportunity_repository.dart';
 import 'package:mosahem/features/splash/presentation/views/splash_view.dart';
@@ -22,6 +25,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
+        RepositoryProvider(
+          create: (_) => ProfileRepository(ProfileApiService(dio)),
+        ),
         RepositoryProvider(create: (_) => AuthRepository(AuthApiService(dio))),
         RepositoryProvider(
           create: (_) =>
@@ -30,6 +36,10 @@ class MyApp extends StatelessWidget {
       ],
       child: MultiBlocProvider(
         providers: [
+          BlocProvider(
+            create: (context) =>
+                ProfileCubit(context.read<ProfileRepository>()),
+          ),
           BlocProvider(
             create: (context) => AuthCubit(context.read<AuthRepository>()),
           ),
